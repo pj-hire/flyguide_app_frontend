7bf71bcf4af8ca84d9b5d17f7b63cc6fa46b1f49
PJ Hire <phire1224@gmail.com>
2021-02-13 10:36:20 -0600

    fb credentials

--- src/main.js
+++ src/main.js
@@ -9,15 +9,6 @@ import '@fortawesome/fontawesome-free/js/all.js'
 import VueGoogleCharts from 'vue-google-charts'
 import '../public/styles.scss'
 
-// Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCD5qaGG_7LBVBC90vOsqwyOr4M3W3ZVHI",
-//   authDomain: "fly-guide-app.firebaseapp.com",
-//   projectId: "fly-guide-app",
-//   storageBucket: "fly-guide-app.appspot.com",
-//   messagingSenderId: "920264779850",
-//   appId: "1:920264779850:web:b9176fa9678638a201deb3"
-// };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
